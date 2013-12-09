4992139149a1e0ffa96794a8388e77bffea7bbc2
bikedude880 <bikedude880@gmail.com>
2013-12-08 20:49:43 -0800

    Sync to CM 10.2 files.

--- extract-files.sh
+++ extract-files.sh
@@ -166,72 +166,212 @@ copy_local_files()
 }
 
 COMMON_LIBS="
-	libril.so
+	libaudiopolicy_sec.so
+	libchromatix_imx074_default_video.so
+	libchromatix_imx074_preview.so
+	libchromatix_imx074_video_hd.so
+	libchromatix_imx074_zsl.so
+	libchromatix_imx091_default_video.so
+	libchromatix_imx091_preview.so
+	libchromatix_imx091_video_hd.so
+	libchromatix_mt9e013_default_video.so
+	libchromatix_mt9e013_preview.so
+	libchromatix_mt9e013_video_hfr.so
+	libchromatix_ov2720_default_video.so
+	libchromatix_ov2720_hfr.so
+	libchromatix_ov2720_preview.so
+	libchromatix_ov2720_zsl.so
+	libchromatix_ov5647_default_video.so
+	libchromatix_ov5647_preview.so
+	libchromatix_ov8825_default_video.so
+	libchromatix_ov8825_preview.so
+	libchromatix_ov9726_default_video.so
+	libchromatix_ov9726_preview.so
+	libchromatix_s5k3l1yx_default_video.so
+	libchromatix_s5k3l1yx_hfr_120fps.so
+	libchromatix_s5k3l1yx_hfr_60fps.so
+	libchromatix_s5k3l1yx_hfr_90fps.so
+	libchromatix_s5k3l1yx_preview.so
+	libchromatix_s5k3l1yx_video_hd.so
+	libchromatix_s5k3l1yx_zsl.so
+	libchromatix_s5k4e1_default_video.so
+	libchromatix_s5k4e1_preview.so
+	libchromatix_s5k6b2yx_pip.so
+	libchromatix_s5k6b2yx_preview.so
+	libchromatix_s5k6b2yx_smartstay.so
+	libchromatix_s5k6b2yx_video.so
+	libchromatix_s5k6b2yx_vt.so
+	libchromatix_s5k6b2yx_vt_hd.so
+	libgemini.so
+	libimage-jpeg-enc-omx-comp.so
+	libimage-omx-common.so
+	libmercury.so
+	libmmcamera_faceproc.so
+	libmmcamera_frameproc.so
+	libmmcamera_hdr_lib.so
+	libmmcamera_image_stab.so
+	libmmcamera_imx091.so
+	libmmcamera_interface.so
+	libmmcamera_interface2.so
+	libmmcamera_plugin.so
+	libmmcamera_statsproc31.so
+	libmmcamera_wavelet_lib.so
+	libmmjpeg.so
+	libmmjpeg_interface.so
+	libmmmpod.so
+	libmmstillomx.so
+	liboemcamera.so
+	libsecnativefeature.so
+	libvdis.so
+	libdrmdecrypt.so
+	libdrmfs.so
+	libdrmtime.so
+	libhdcp2.so
+	libQSEEComAPI.so
+	libWVStreamControlAPI_L1.so
+	libwvm.so
+	libqc-opt.so
+	libsensirion_j1.so
+	libsam.so
+	libloc_api_v02.so
+	libatparser.so
+	libcordon.so
+	libdiag.so
+	libdsutils.so
+	libdsi_netctrl.so
+	libfactoryutil.so
+	libidl.so
+	libnetmgr.so
+	libomission_avoidance.so
+	libqdi.so
+	libqdp.so
+	libqmi.so
+	libqmiservices.so
+	libqmi_cci.so
+	libqmi_common_so.so
+	libqmi_csi.so
+	libqmi_encdec.so
+	libqcci_legacy.so
+	libqmi_client_qmux.so
+	libreference-ril.so
+	libril-qcril-hook-oem.so
 	libsecril-client.so
-	libtvoutservice.so
-	libtvoutinterface.so
-	libdirencryption.so
-	libext2_blkid.so
-	libext2_com_err.so
-	libext2_e2p.so
-	libext2fs.so
-	libext2_uuid.so
-	libkeyutils.so
-	libsec_devenc.so
-	libsec_ecryptfs.so
-	libsecfips.so
-	libsec_km.so
-	libsurfaceflinger.so
+	libtime_genoff.so
+	libmm-color-convertor.so
+	libC2D2.so
+	libOpenVG.so
+	libOpenCL.so
+	libsc-a2xx.so
+	libsc-a3xx.so
+	libllvm-a3xx.so
+	libExtendedExtractor.so
+	libmmparser.so
+	libmmosal.so
+	libdivxdrm.so
+	libacdbloader.so
+	libaudcal.so
+	libcsd-client.so
+	libalsa-intf.so
+	libc2dcolorconvert.so
+	libdashplayer.so
+	libdivxdrmdecrypt.so
+	libexternal.so
+	libgenlock.so
+	libmemalloc.so
+	libmm-omxcore.so
+	libOmxAacEnc.so
+	libOmxAmrEnc.so
+	libOmxCore.so
+	libOmxEvrcEnc.so
+	libOmxQcelp13Enc.so
+	libOmxVdec.so
+	libOmxVenc.so
+	liboverlay.so
+	libqdMetaData.so
+	libqdutils.so
+	libqservice.so
+	libstagefrighthw.so
 	"
-
 copy_files "$COMMON_LIBS" "system/lib" ""
 
 COMMON_BINS="
-	playlpm
-	immvibed
-	lpmkey
+	qseecomd
+	ds_fmc_appd
+	efsks
+	ks
+	netmgrd
+	sec-ril
+	qcks
+	qmiproxy
+	qmuxd
 	rild
-	mediaserver
-	servicemanager
-	vold
-	${HCDNAME}
-	surfaceflinger
-	bintvoutservice
+	rmt_storage
+	mm-qcamera-daemon
+	thermald
+	thermal-engine
+	mpdecision
+	mm-vdec-omx-test
+	mm-venc-omx-test720p
+	mm-video-driver-test
+	mm-video-encdrv-test
 	"
 copy_files "$COMMON_BINS" "system/bin" ""
 
 COMMON_CAMERADATA="
+	M10MO_SFW.bin
+	RS_M10MO_OL.bin
+	RS_M10MO_OS.bin
+	RS_M10MO_SL.bin
+	RS_M10MO_SS.bin
 	datapattern_420sp.yuv
 	datapattern_front_420sp.yuv
 	"
 copy_files "$COMMON_CAMERADATA" "system/cameradata" "cameradata"
 
 COMMON_EGL="
-	egl.cfg
+	eglsubAndroid.so
+	libGLES_android.so
+	libGLESv2S3D_adreno200.so
 	libEGL_adreno200.so
 	libGLESv1_CM_adreno200.so
 	libGLESv2_adreno200.so
+	libq3dtools_adreno200.so
 	"
 copy_files "$COMMON_EGL" "system/lib/egl" "egl"
 
-if [ $FIRMWARE = "XXLPQ" ] || [ $FIRMWARE = "XWLP7" ];
-then
 COMMON_FIRMWARE="
-	RS_M5LS_TB.bin
+	a225p5_pm4.fw
+	a225_pfp.fw
+	a225_pm4.fw
+	a300_pfp.fw
+	a300_pm4.fw
+	leia_pfp_470.fw
+	leia_pm4_470.fw
+	vidc_1080p.fw
+	wcd9310/wcd9310_anc.bin
+	wcd9310/wcd9310_mbhc.bin
 	"
-fi
-
 copy_files "$COMMON_FIRMWARE" "system/etc/firmware" "firmware"
 
+VENDOR_FIRMWARE="
+	bcm2079xB4_firmware.ncd
+	bcm2079xB4_pre_firmware.ncd
+	bcm4335.hcd
+	bcm4335_A0.hcd
+	bcm4335_murata.hcd
+	bcm4335_semco.hcd
+	"
+copy_files "$VENDOR_FIRMWARE" "vendor/firmware" "vendor/firmware"
+
 COMMON_HW="
-	audio.a2dp.default.so
-	audio_policy.default.so
-	audio.primary.default.so
-	audio.primary.msm8960.so
-	audio.primary.goldfish.so
 	camera.msm8960.so
-	gralloc.msm8960.so
 	sensors.msm8960.so
+	sensorhubs.msm8960.so
+	audio.primary.msm8960.so
+	audio_policy.msm8960.so
+	copybit.msm8960.so
+	gralloc.msm8960.so
+	hwcomposer.msm8960.so
 	"
 copy_files "$COMMON_HW" "system/lib/hw" "hw"
 
@@ -251,11 +391,24 @@ copy_files "$COMMON_KEYCHARS" "system/usr/keychars" "keychars"
 
 COMMON_WIFI="
 	bcmdhd_apsta.bin
+	bcmdhd_apsta.bin_a0
 	bcmdhd_mfg.bin
+	bcmdhd_mfg.bin_a0
 	bcmdhd_sta.bin
+	bcmdhd_sta.bin_a0
+	cred.conf
 	nvram_mfg.txt
+	nvram_mfg.txt_a0
+	nvram_mfg.txt:etc/wifi/nvram_mfg.txt_murata
+	nvram_mfg.txt_murata_a0
+	nvram_mfg.txt_muratafem1
+	nvram_mfg.txt_muratafem2
+	nvram_mfg.txt_semco3rd
+	nvram_mfg.txt_semco3rd_a0
+	nvram_mfg.txt_semcosh
 	nvram_net.txt
 	nvram_net.txt_a0
+	nvram_net.txt:etc/wifi/nvram_net.txt_murata
 	nvram_net.txt_murata_a0
 	nvram_net.txt_muratafem1
 	nvram_net.txt_muratafem2
