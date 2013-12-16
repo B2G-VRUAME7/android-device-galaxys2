9dd28a148ec1afba4e2a1f1b7547c5209c0fac1b
bikedude880 <bikedude880@gmail.com>
2013-12-15 19:18:15 -0800

    Sync to CM10.2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,22 +17,16 @@
 # Product-specific compile-time definitions.
 #
 
-# Set this up here so that BoardVendorConfig.mk can override it
-BOARD_USES_GENERIC_AUDIO := false
-BOARD_PREBUILT_LIBAUDIO := true
-
-BOARD_USES_LIBSECRIL_STUB := true
-
 # Use the non-open-source parts, if they're present
--include vendor/samsung/galaxys2/BoardConfigVendor.mk
+-include vendor/samsung/galaxys4/BoardConfigVendor.mk
 
+TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
-ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
@@ -40,50 +34,31 @@ TARGET_NO_RADIOIMAGE := true
 TARGET_PROVIDES_INIT := true
 TARGET_PROVIDES_INIT_RC := true
 TARGET_PROVIDES_INIT_TARGET_RC := true
-TARGET_BOARD_PLATFORM := exynos4
-TARGET_BOOTLOADER_BOARD_NAME := GT-I9100
-TARGET_RECOVERY_INITRC := device/samsung/c1-common/recovery.rc
+TARGET_BOARD_PLATFORM := msm8960
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 TARGET_PROVIDES_MEDIASERVER := true
 
-BOARD_MOBILEDATA_INTERFACE_NAME = "pdp0"
 
 # Releasetools
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/c1-common/releasetools/c1_ota_from_target_files
 TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/c1-common/releasetools/c1_img_from_target_files
 
 # Camera
-USE_CAMERA_STUB := false
-ifeq ($(USE_CAMERA_STUB),false)
-BOARD_CAMERA_LIBRARIES := libcamera
-TARGET_GLOBAL_CPPFLAGS += -DUSE_GS2_LIBCAMERA
-endif
+USE_CAMERA_STUB := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 
-# gps
-BOARD_USES_GPSWRAPPER := true
-
-# FM Radio
-BOARD_HAVE_FM_RADIO := true
-BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
-BOARD_FM_DEVICE := si4709
-
-# Vibrator
-BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/c1-common/vibrator/tspdrv.c
-
-#BOARD_NAND_PAGE_SIZE := 4096 -s 128
-BOARD_NAND_PAGE_SIZE := 4096
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_BASE := 0x40000000
-BOARD_KERNEL_CMDLINE := console=ttySAC2,115200 consoleblank=0
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_BASE := 0x80200000
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache
 TARGET_PREBUILT_KERNEL := device/samsung/galaxys4/kernel
 
-BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 536870912
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
-BOARD_FLASH_BLOCK_SIZE := 4096
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 28651290624
+BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Connectivity - Wi-Fi
 #BOARD_WPA_SUPPLICANT_DRIVER := NL80211
@@ -100,19 +75,17 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 #WIFI_DRIVER_MODULE_ARG      :=  "firmware_path=/system/etc/wifi/bcm4330_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
 
 # Vold
-BOARD_VOLD_MAX_PARTITIONS := 12
+BOARD_VOLD_MAX_PARTITIONS := 28
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxys4/recovery/recovery_ui.c
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxys4/recovery/recovery_keys.c
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/c1-common/shbootimg.mk
-
-# assert
-TARGET_OTA_ASSERT_DEVICE := galaxys2,GT-I9100,GT-I9100M,GT-I9100T
+# Assert
+TARGET_OTA_ASSERT_DEVICE := jfltevzw
 
 # Include aries specific stuff
 -include device/samsung/c1-common/Android.mk
