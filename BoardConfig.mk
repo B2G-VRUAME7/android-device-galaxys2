6f8997af9a5b93a2ad6816c78b8305db408f4d94
Daniel Penney <dpenney0114@gmail.com>
2013-12-16 22:19:36 -0800

    Sync CM changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,72 +20,61 @@
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/galaxys4/BoardConfigVendor.mk
 
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8960
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# Arch/CPU
 TARGET_ARCH := arm
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+
+# Flags
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64 -DQCOM_BSP_CAMERA_ABI_HACK
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
+# Init
 TARGET_PROVIDES_INIT := true
 TARGET_PROVIDES_INIT_RC := true
 TARGET_PROVIDES_INIT_TARGET_RC := true
-TARGET_BOARD_PLATFORM := msm8960
-TARGET_BOOTLOADER_BOARD_NAME := MSM8960
-TARGET_PROVIDES_MEDIASERVER := true
 
+# Kernel
+TARGET_PREBUILT_KERNEL := device/samsung/galaxys4/kernel
+BOARD_KERNEL_BASE           := 0x80200000
+BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom user_debug=31 zcache
+BOARD_KERNEL_PAGESIZE       := 2048
 
 # Releasetools
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/c1-common/releasetools/c1_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/c1-common/releasetools/c1_img_from_target_files
-
-# Camera
-USE_CAMERA_STUB := true
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_BASE := 0x80200000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache
-TARGET_PREBUILT_KERNEL := device/samsung/galaxys4/kernel
-
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 28651290624
-BOARD_FLASH_BLOCK_SIZE := 131072
-
-# Connectivity - Wi-Fi
-#BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-#WPA_SUPPLICANT_VERSION := VER_0_8_X
-#BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-#BOARD_HOSTAPD_DRIVER        := NL80211
-#BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
-#OARD_WLAN_DEVICE           := bcmdhd
-#WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
-#WIFI_DRIVER_MODULE_PATH     := "/lib/modules/dhd.ko"
-#WIFI_DRIVER_FW_STA_PATH     := "/system/etc/wifi/bcm4330_sta.bin"
-#WIFI_DRIVER_FW_AP_PATH      := "/system/etc/wifi/bcm4330_aps.bin"
-#IFI_DRIVER_MODULE_NAME     :=  "dhd"
-#WIFI_DRIVER_MODULE_ARG      :=  "firmware_path=/system/etc/wifi/bcm4330_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+#TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/galaxys4/releasetools/ota_from_target_files
+#TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/galaxys4/releasetools/img_from_target_files
 
 # Vold
 BOARD_VOLD_MAX_PARTITIONS := 28
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 28651290624
+BOARD_FLASH_BLOCK_SIZE := 131072
+
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxys4/recovery/recovery_keys.c
 BOARD_USES_MMCUTILS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := jfltevzw
-
-# Include aries specific stuff
--include device/samsung/c1-common/Android.mk
